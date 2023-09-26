4d0be323ee17f5b0f9e8eb01203b2c14740d6e40
William Ajspur <wilajs23@gmail.com>
2023-09-26 12:16:50 +0200

    Creating variables and methods in game class

--- src/Game.java
+++ src/Game.java
@@ -1,3 +1,26 @@
 public class Game {
+    private Player player1;
+    private Player player2;
+    private Cup cup;
+    private int turnCount;
 
+    public void startGame(){
+
+    }
+
+    public void playGame(){
+
+    }
+
+    public void displayRules(){
+
+    }
+
+    public void playerTurn(Player player, Cup cup){
+
+    }
+
+    public void displayScoreboard(){
+        
+    }
 }
