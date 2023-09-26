248fef6645f96f1ee6b25d2b071baa51130f64ac
Julius Sondergaard <sondergaardjulius@gmail.com>
2023-09-26 12:46:46 +0200

    PlayerFile1

--- src/Player.java
+++ src/Player.java
@@ -1,3 +1,26 @@
-public class Player {
+class Player {
 
+    private String name;
+    private int score;
+
+    public Player(String name, int score) {
+        this.name = name;
+        this.score = score;
+    }
+
+    public void setPlayerName(String name) {
+        this.name = name;
+    }
+
+    public String getPlayerName() {
+        return this.name;
+    }
+
+    public void setScore(int currentrollSum) {
+        this.score = currentrollSum;
+    }
+
+    public int getScore() {
+        return this.score;
+    }
 }
