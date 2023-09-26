4d0a334142b2aa4203adf6eb50f7e8b7ddce9167
Lau <mikkel2011@hotmail.com>
2023-09-26 14:30:20 +0200

    Die v2, randomizing number

--- src/Die.java
+++ src/Die.java
@@ -1,3 +1,15 @@
 public class Die {
+private int dieValue;
+
+
+public Die(){
+}
+
+public void setDieValue(int dieValue) {
+    dieValue=(int)Math.floor(Math.random()*(6-1+1)+1);
+}
+public int getDieValue(){
+    return dieValue;
+}
 
 }
