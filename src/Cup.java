f2f500c06ecbf5db250a7cb7b6c6e41b374a0b0d
RachidAM <moutiq@gmail.com>
2023-09-26 14:31:21 +0200

    Cup-klasse udkast

--- src/Cup.java
+++ src/Cup.java
@@ -1,3 +1,46 @@
 public class Cup {
+    
+    private Die die1 = new Die(); 
+    private Die die2 = new Die();
+    
+    private int currentRollSum;  
+    
+    public Cup() {
+    this.currentRollSum = 0;
+    }
+    public void setRollSum(int newRollSum){
+        die1.setDieValue();
+        die2.setDieValue();
+        this.currentRollSum = die1.getDieValue + die2.getDieValue;
+    }
+    public int getRollSum (){
+        return this.currentRollSum;
+    }
+    public int getDie1(){
+        return die1.getDieValue; 
+    }
+    public int getDie2(){
+        return die2.getDieValue; 
+    }    
+}   
+
+    /* Her er teksten fra klasse-diagrammet RM
+   
+- die1 : Die
+- die2 : Die
+
+- currentRollSum : int
+
+
+
++ Cup()
+
++ setRollSum() : void
+
++ getRollSum() : int
++ getDie1() : int
+
++ getDie2() : int
+ */
 
 }
