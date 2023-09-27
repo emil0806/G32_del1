e33a695f10180e677f9b4d08bb98ee297770b616
Emil Lauritzen <emil6008@gmail.com>
2023-09-27 16:53:38 +0200

    fix syntax and add comments in testTime

--- src/test/TimeTest.java
+++ src/test/TimeTest.java
@@ -11,16 +11,24 @@ public class TimeTest {
     private Player player = new Player("", 0);
     private Cup cup = new Cup();
 
+    /*
+     * Test the elapsed time from when playerturn() is called until finished.
+     * If this it below 333 milliseconds, the program definetely prints out the
+     * result of a roll
+     * in less than 333 milliseconds.
+     */
     @Test
-    public void TestTime() {
-
+    public void testTime() {
+        // Start timer
         long startTime = System.nanoTime();
 
         newGame.playerTurn(player, cup);
 
+        // End timer
         long endTime = System.nanoTime();
+        // Calculate elapsed time milliseconds
         double totalTime = (endTime - startTime) / (double) 1000000.0;
-
+        // Compare to 333 milliseconds
         Assert.assertTrue(333 > totalTime);
     }
 
